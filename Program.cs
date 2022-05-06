c4aeb6ee19b0845482c67b58ea00e7fab6491ffc
paul <outimer@mail.ru>
2022-05-06 14:19:20 +0600

    Decompose and README with solution

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// формирование результирующего массива строк, длина которых меньше либо равна lim символов
+string[] GetFormedArray(string[] array, int lim)
+{
+    string[] resArray = {};
+
+    return resArray;
+}
+
+// вывод массива
+string PrintArray(string[] arr)
+{
+    string result = string.Empty;    
+
+    return result;
+}
+
+
+Console.Clear();
+string[] initialArray = {"hello", "2", "world", ":-)"};  // исходный массив строк
+int trim = 3;  // условие на длину строк результирующего массива
+
+
+Console.Write(PrintArray(initialArray) + " -> ");
+string[] resultArray = GetFormedArray(initialArray, trim);
+Console.WriteLine(PrintArray(resultArray));
+
+// вариант вывода в файл проекта
+File.WriteAllText("res.txt", PrintArray(initialArray) + " -> " + PrintArray(resultArray));
